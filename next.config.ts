05a4c824cd6f476f935d8941c8849f8c4b947457
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-09-14 17:13:31 +0300

    feat: make gallery responsive

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  sassOptions: {
+    loadPaths: [path.join(process.cwd(), "src")],
+  },
   images: {
     remotePatterns: [
       {
