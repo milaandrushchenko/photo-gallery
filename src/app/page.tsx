8cf0b4bc8050ac342ec5dbea2a52c9b9f8b3e6f3
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-09-14 16:26:20 +0300

    style: add base page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { Gallery } from "@/components/Gallery/Gallery";
 import { getPhotos } from "@/lib/unsplash/client";
 
+import styles from "./page.module.scss";
+
 export default async function HomePage() {
   const photos = await getPhotos();
 
   return (
-    <main>
-      <h1>Photo Gallery</h1>
+    <main className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Photo Gallery</h1>
 
-      <Gallery photos={photos} />
+        <Gallery photos={photos} />
+      </div>
     </main>
   );
 }
